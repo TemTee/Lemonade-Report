7b25e5264f4504cc582849439989a5d0f30a84f3
TemTee <temmyteeliz@gmail.com>
2021-04-20 17:22:59 +0100

    adjustment

--- Code.R
+++ Code.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(dplyr)
 library(gridExtra)
 library(lubridate)
+library(caret)
 library(psych)
 
 #          *******            Load and manipulate the data        ***********
@@ -35,13 +36,16 @@ head(lemonade)
 
 #####                   *******     Exploring the data          **********
 
-plot(lemonade$Month, lemonade$Sales)
-plot(lemonade$Day, lemonade$Sales)
+####### Visualize data
 
-####### Visualize distribution of sales
+lot(lemonade$Month, lemonade$Sales)
+plot(lemonade$Day, lemonade$Sales)
 plot(lemonade)
 hist(lemonade$Sales)
-
+lemonade %>% ggplot(aes(Temperature_in_Cel, Sales, color = Month)) +
+  geom_point()+
+  geom_jitter()
+grid.arrange()
 # summary of distribution of the variables
 
 lemonade %>% 
@@ -59,12 +63,13 @@ round(cor(subset(lemonade, select = c(-Date, -Month, -Day)), method = "pearson")
 round(sapply(data.frame(subset(lemonade, select = c(-Date, -Month, -Day))),
        cor, y = lemonade$Sales, method = "pearson"), digits = 2)
 
-# to  standardize the data using z score
+pairs.panels(lemonade)
+
+
+# to  standardize the data using z score, run below code
 
 # lemonade_z <- as.data.frame(scale(lemonade[4:8])) 
-
 # add other missing variables from lemonade
-
 # lemonade_z <- data.frame(lemonade[c(1,2,3)], lemonade_z) 
 
 
@@ -73,9 +78,9 @@ round(sapply(data.frame(subset(lemonade, select = c(-Date, -Month, -Day))),
 
 ##################### split the data
 
-library(caret)
 set.seed(1, sample.kind = "Rounding")
-index <- with(lemonade, createDataPartition(y= lemonade$Sales, times = 1, p = 0.3, list = FALSE))
+index <- with(lemonade, createDataPartition(y= lemonade$Sales, 
+                                            times = 1, p = 0.3, list = FALSE))
 train_set <- lemonade [-index,]
 test_set <- lemonade [index, ]
 str(train_set)
@@ -84,7 +89,7 @@ str(test_set)
 
 #####################  train the model
 
-fit <- lm(Sales ~ Temperature_in_Cel + Rainfall +
+fit <- lm(Sales ~ Temperature_in_Cel +
             Flyers + Temperature_in_Cel*Flyers + 
             Rainfall*Flyers + Month, data = train_set)
 
@@ -94,14 +99,16 @@ prediction <- predict(fit, test_set)
 
 summary(fit)
 
-# calculate the error rate of the model using the residual standard error 74.2
+# calculate the error rate of the model using the residual standard error 73.6
+# error rate of the model turn out to be at 3%
 
-74.2 / mean(train_set$Sales) 
+73.6 / mean(train_set$Sales) 
 
-#  our rmse using the psych package appears ok for a 0 to over four thousand range
+#  our rmse using the psych package appear to be at 76 for a sale value
+#   ranging from 704 - 4322 range
 
 RMSE(prediction, test_set$Sales)
-RMSE(fit, train_set$Sales)
+RMSE(fit$fitted.values, train_set$Sales)
 
 # our r ssquared is also at %
 R2(prediction, test_set$Sales)
